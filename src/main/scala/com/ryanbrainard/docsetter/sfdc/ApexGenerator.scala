2fcb110eaafe5a2142208b5daffc2f0751b6a1f9
Ryan Brainard <brainard@heroku.com>
2013-09-13 00:16:55 -0700

    Customize Apex entry types

--- src/main/scala/com/ryanbrainard/docsetter/sfdc/ApexGenerator.scala
+++ src/main/scala/com/ryanbrainard/docsetter/sfdc/ApexGenerator.scala
@@ -1,8 +1,16 @@
 package com.ryanbrainard.docsetter.sfdc
 
-import java.net.URL
+import com.ryanbrainard.docsetter.EntryType
+import scala.collection.immutable.ListMap
 
 class ApexGenerator extends AbstractSfdcGenerator {
   override val sfdcId = "apexcode"
   override val name   = "Apex"
+  override val entryTypeRegexMappings = ListMap(
+    "Exposing Data with Apex REST Web Service Methods"  -> defaultEntryType,
+    "Exposing Data with WebService Methods"  -> defaultEntryType,
+    "Mixed DML Operations in Test Methods"  -> defaultEntryType,
+    "Overloading Web Service Methods"  -> defaultEntryType,
+    ".* Annotation"  -> EntryType.Notation
+  ) ++ super.entryTypeRegexMappings
 }
