3770ec07546be9a74970c011c7ea7800dcc1a103
Ryan Brainard <ryan.brainard@gmail.com>
2013-09-20 21:45:06 -0700

    sort of working offline heroku devcenter gen

--- src/main/scala/com/ryanbrainard/docsetter/heroku/DevcenterGenerator.scala
+++ src/main/scala/com/ryanbrainard/docsetter/heroku/DevcenterGenerator.scala
@@ -4,7 +4,7 @@ import com.ryanbrainard.docsetter.{EntryType, IndexEntry, Generator}
 import java.net.URL
 import scala.io.Source
 import scala.util.parsing.json.{JSONObject, JSONArray, JSON}
-import java.io.{PrintWriter, File}
+import java.io.{FileInputStream, FileOutputStream, PrintWriter, File}
 
 class DevcenterGenerator extends Generator {
 
@@ -20,26 +20,50 @@ class DevcenterGenerator extends Generator {
 
   def iconUrl = new URL("https://raw.github.com/heroku/favicon/master/favicon.iconset/icon_32x32.png")
 
-  def index(docsDir: File) = loadJson(url("/api/v1/articles.json", secure = true)).collect {
-    case articles: JSONArray =>
-      articles.list.par.collect {
-        case article: JSONObject =>
-          val apiUrl = new URL(article.obj("api_url").toString)
-          loadJson(apiUrl).collect {
-            case data: JSONObject =>
-              val slug = data.obj("slug")
-              val title = data.obj("title")
-              val htmlContent = data.obj("html_content")
-              val path = slug + ".html"
-              val file = new File(docsDir, path)
-              val writer = new PrintWriter(file)
-              writer.print(htmlContent)
-              writer.close()
-              println("wrote:" + Thread.currentThread() + file)
-              IndexEntry(title.toString, EntryType.Guide, path.toString)
-          }.getOrElse(sys.error("Could not load article from " + apiUrl))
-      }.seq
-  }.getOrElse(sys.error("Could not build Heroku Devcenter index"))
+  val templateUrl= getClass.getClassLoader.getResource("heroku-devcenter/dynamic/layout.html")
+
+  val staticAssetsResourcesDir = new File(getClass.getClassLoader.getResource("heroku-devcenter/static/assets").getFile)
+
+  def index(docsDir: File) = {
+    val articlesDir = new File(docsDir, "articles")
+    val assetsDir = new File(docsDir, "assets")
+    articlesDir.mkdirs()
+    assetsDir.mkdirs()
+
+    val template = Source.fromURL(templateUrl).getLines().mkString
+
+    staticAssetsResourcesDir.listFiles().filter(_.isFile).foreach(f => copy(f, new File(assetsDir, f.getName)))
+
+    loadJson(url("/api/v1/articles.json", secure = true)).collect {
+      case articles: JSONArray =>
+        articles.list.par.collect {
+          case article: JSONObject =>
+            val apiUrl = new URL(article.obj("api_url").toString)
+            loadJson(apiUrl).collect {
+              case data: JSONObject =>
+                println("writing:" + Thread.currentThread() + apiUrl)
+
+                val slug = data.obj("slug").toString + ".html"
+                val title = data.obj("title").toString
+                val meta = data.obj("meta_description").toString
+                val htmlContent = data.obj("html_content").toString
+                val path = Seq(articlesDir.getName, slug).mkString("/")
+                val file = new File(articlesDir, slug)
+                val writer = new PrintWriter(file)
+                val html = template
+                  .replaceAllLiterally("{{TITLE}}", title)
+                  .replaceAllLiterally("{{META}}", meta)
+                  .replaceAllLiterally("{{BODY}}", htmlContent)
+                  .replaceAllLiterally("\"/articles/", "\"")
+
+                writer.print(html)
+                writer.close()
+                println("wrote:" + Thread.currentThread() + file)
+                IndexEntry(title, EntryType.Guide, path)
+            }.getOrElse(sys.error("Could not load article from " + apiUrl))
+        }.seq
+    }.getOrElse(sys.error("Could not build Heroku Devcenter index"))
+  }
 
   private def loadJson(url: URL) = {
     JSON.parseRaw(Source.fromURL(url).getLines().mkString)
@@ -47,5 +71,16 @@ class DevcenterGenerator extends Generator {
 
   private def url(path: String, secure: Boolean = false) = {
     new URL("http" + (if (secure) "s" else "") + "://" + host + path)
+  }
+
+  def copy(from: File, to: File) = {
+    val fileInputStream = new FileInputStream(from)
+    val fileOutputStream = new FileOutputStream(to)
+    var buffer = -1
+    while (-1 != {buffer = fileInputStream.read; buffer}) {
+      fileOutputStream.write(buffer)
+    }
+    fileInputStream.close()
+    fileOutputStream.close()
   }
 }
